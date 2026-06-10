e5afdc2269ec68fa66fd2794c4291eb3bc6f3bdf
Anthony Barrows <ajbarrow@uvm.edu>
2026-06-10 10:56:31 -0400

    reconcile

--- src/config/papers.ts
+++ src/config/papers.ts
@@ -4,56 +4,48 @@ export const papersContent = {
     description: "Peer-reviewed publications",
   },
   title: "Papers",
-  // description:
-  //   "Peer-reviewed publications.",
   papers: [
     {
-      title:
-        "Cognitive Process Models Reveal Meaningful Brain–Behavior Associations in the Adolescent Brain Cognitive Development Study Stop-signal Task",
+      title: "Cognitive Process Models Reveal Meaningful Brain–Behavior Associations in the Adolescent Brain Cognitive Development Study Stop-signal Task",
       authors: "Barrows A, Weigard A, McCabe M, Potter A, Garavan H, Allgaier N",
       citation: "Journal of Cognitive Neuroscience, 2026",
-      image: "/assets/images/figures/BaWe26.jpg",
+      image: "",
       href: "https://doi.org/10.1162/JOCN.a.2487",
     },
     {
-      title:
-        "Smoking reduction trajectories and their association with smoking cessation: a secondary analysis of longitudinal clinical trial data",
+      title: "Smoking Reduction Trajectories and Their Association with Smoking Cessation: A Secondary Analysis of Longitudinal Clinical Trial Data",
       authors: "Barrows A, Klemperer E, Garavan H, Allgaier N, Lindson N, Taylor G",
       citation: "BMJ Public Health, 3(2), 2025",
       image: "/assets/images/figures/BaKl25.jpg",
-      href: "http://dx.doi.org/10.1136/bmjph-2024-001605",
+      href: "https://doi.org/10.1136/bmjph-2024-001605",
     },
     {
-      title:
-        "The Impact of the Covid-19 Pandemic on Smoking Among Vulnerable Populations",
-      authors: "Wiley RC, Oliver AC, Snow MB, Bunn JY, Barrows AJ, … Higgins ST",
+      title: "The Impact of the Covid-19 Pandemic on Smoking Among Vulnerable Populations",
+      authors: "Wiley R, Oliver A, Snow M, Bunn J, Barrows A, Tidey J, Lee D, Sigmon S, Gaalema D, Heil S, Markesich C, Villanti A, Higgins S",
       citation: "Nicotine & Tobacco Research, 25(2), 2022",
       image: "/assets/images/figures/WiOl22.jpg",
-      href: "http://dx.doi.org/10.1093/ntr/ntac135",
+      href: "https://doi.org/10.1093/ntr/ntac135",
     },
     {
-      title:
-        "Changes in Cigarette Consumption With Reduced Nicotine Content Cigarettes Among Smokers With Psychiatric Conditions or Socioeconomic Disadvantage: 3 Randomized Clinical Trials",
-      authors: "Higgins ST, Tidey JW, … Barrows A, …",
+      title: "Changes in Cigarette Consumption With Reduced Nicotine Content Cigarettes Among Smokers With Psychiatric Conditions or Socioeconomic Disadvantage",
+      authors: "Higgins S, Tidey J, Sigmon S, Heil S, Gaalema D, Lee D, Hughes J, Villanti A, Bunn J, Davis D, Bergeria C, Streck J, Parker M, Miller M, DeSarno M, Priest J, Cioe P, MacLeod D, Barrows A, Markesich C, Harfmann R",
       citation: "JAMA Network Open, 3(10), 2020",
       image: "/assets/images/figures/HiTi20.jpg",
-      href: "http://dx.doi.org/10.1001/jamanetworkopen.2020.19311",
+      href: "https://doi.org/10.1001/jamanetworkopen.2020.19311",
     },
     {
-      title:
-        "Comparing participant estimated demand intensity on the cigarette Purchase Task to consumption when usual-brand cigarettes were provided free",
-      authors: "Nighbor TD, Barrows AJ, Bunn JY, DeSarno MJ, … Higgins ST",
+      title: "Comparing Participant Estimated Demand Intensity on the Cigarette Purchase Task to Consumption when Usual-Brand Cigarettes were Provided Free",
+      authors: "Nighbor T, Barrows A, Bunn J, DeSarno M, Oliver A, Coleman S, Davis D, Streck J, Reed E, Reed D, Higgins S",
       citation: "Preventive Medicine, 140, 2020",
       image: "/assets/images/figures/TyAn20.jpg",
       href: "https://doi.org/10.1016/j.ypmed.2020.106221",
     },
     {
-      title:
-        "Reduced Nicotine Content Cigarettes and Cannabis Use in Vulnerable Populations",
-      authors: "Parker MA, Streck JM, Bergeria CL, Bunn JY, … Barrows AJ, … Higgins ST",
+      title: "Reduced Nicotine Content Cigarettes and Cannabis Use in Vulnerable Populations",
+      authors: "Parker M, Streck J, Bergeria C, Bunn J, Gaalema D, Davis D, Barrows A, Sigmon S, Tidey J, Heil S, Higgins S",
       citation: "Tobacco Regulatory Science, 4(5), 2018",
       image: "/assets/images/figures/PaSt18.jpg",
-      href: "http://dx.doi.org/10.18001/TRS.4.5.8",
+      href: "https://doi.org/10.18001/TRS.4.5.8",
     },
   ],
 };
